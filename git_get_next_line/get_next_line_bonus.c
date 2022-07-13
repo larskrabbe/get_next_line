10ee78e452dd2caafe88b9d574408b9802ce63d2
Lars Krabbe <lkrabbe@3-B-13.42heilbronn.de>
2022-07-13 19:07:22 +0200

    13.7.2022 just norminette left

--- git_get_next_line/get_next_line_bonus.c
+++ git_get_next_line/get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrabbe <lkrabbe@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 19:27:23 by lkrabbe           #+#    #+#             */
-/*   Updated: 2022/07/13 19:05:56 by lkrabbe          ###   ########.fr       */
+/*   Updated: 2022/07/13 19:04:01 by lkrabbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"get_next_line.h"
+#include	"get_next_line_bonus.h"
 #include	<stdio.h> //del later
 
 int	lookfor(int fd, char *buffer, char *tmp_buf)
@@ -58,7 +58,7 @@ char *rec_str_join(int fd,char *ptr,size_t size,char *tmp_buf)
 			return(NULL);
 		ptr[size  + i] = '\0';
 	}
-	while (i-- > 0)
+	while ( i-- > 0)
 		ptr[size + i] = buffer[i];
 	return (ptr);
 }
@@ -66,18 +66,19 @@ char *rec_str_join(int fd,char *ptr,size_t size,char *tmp_buf)
 char	*get_next_line(int fd)
 {
 	char		*ptr = NULL;
-	static char sta_buf[BUFFERSIZE];
+	static char sta_buf[4096][BUFFERSIZE + 1];
 	int			i;
 	char		tmp_buf[BUFFERSIZE] = "";
 	int 		k;
 	
 	k = 0;
 	i = 0;
+	
 	if (fd > 4096 || fd < 0)
 		return (NULL);
-	while(sta_buf[i] != '\0' && sta_buf[i] != '\n')
+	while(sta_buf[fd][i] != '\0' && sta_buf[fd][i] != '\n')
 		i++;
-	if (sta_buf[i] == '\n')
+	if (sta_buf[fd][i] == '\n')
 	{
 		i++;
 		ptr = malloc(sizeof(char) * (i + 1));
@@ -85,20 +86,20 @@ char	*get_next_line(int fd)
 			return (NULL);
 		while (i > k)
 		{
-			ptr[k] = sta_buf[k];
+			ptr[k] = sta_buf[fd][k];
 			k++;
 		}
 		ptr[k] = '\0';
 		k = 0;
 		while (k + i < BUFFERSIZE)
 		{
-			sta_buf[k] = sta_buf[k+i];
+			sta_buf[fd][k] = sta_buf[fd][k+i];
 			k++;
 		}
 
 		while (i >= 0)
 		{
-			sta_buf[k+i] = '\0';
+			sta_buf[fd][k+i] = '\0';
 			i--;
 		}	
 		return (ptr);
@@ -106,17 +107,17 @@ char	*get_next_line(int fd)
 	ptr = rec_str_join(fd, ptr, i, &(*tmp_buf));
 	while ( i--  > 0)
 		{
-			ptr[i] = sta_buf[i];
-			sta_buf[i] = '\0';
+			ptr[i] = sta_buf[fd][i];
+			sta_buf[fd][i] = '\0';
 		}
 	while (tmp_buf[k] != '\0')
 	{
-		sta_buf[k] = tmp_buf[k];
+		sta_buf[fd][k] = tmp_buf[k];
 		k++;
 	}
 	while ( k <= BUFFERSIZE)
 	{
-		sta_buf[k] = '\0';
+		sta_buf[fd][k] = '\0';
 		k++;
 	}
 	return(ptr);
